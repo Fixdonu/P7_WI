c5eaa0a5484a2150fc82aedd0e2c690f7939f274
Jonathan Magnussen <jnma14@student.aau.dk>
2017-09-27 12:55:53 +0200

    Updated Regex

--- MyCrawler/MyCrawler/Crawler.cs
+++ MyCrawler/MyCrawler/Crawler.cs
@@ -17,6 +17,7 @@ namespace MyCrawler
         string crawlerName;
         LinkedList<string> frontierP = new LinkedList<string>();
         int crawlerDelay = 1000;
+        Dictionary<string, Regex> Regexes;
 
         public Crawler(RobotTxtParser rtp, string crawlerName)
         {
@@ -24,6 +25,51 @@ namespace MyCrawler
             this.rtp = rtp;
             lp = new LinkParser(rtp);
             frontierP.AddLast("http://cknotes.com");
+
+            this.Regexes = new Dictionary<string, Regex>
+                            {
+                                {
+                                    "links",
+                                    new Regex("<a.*?href=(['\"])(?<link>.*?)\\1.*?",
+                                              RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Singleline)
+                                },
+                                {
+                                    "body",
+                                    new Regex("<body(?:[ ][^>]*)?>(?<contents>.*?)</body>",
+                                              RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Singleline)
+                                },
+                                {
+                                    "scripts",
+                                    new Regex("<script[^>]*>.*?</script>",
+                                              RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Singleline)
+                                },
+                                {
+                                    "styles",
+                                    new Regex("<style[^>]*>.*?</style>",
+                                    RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                                },
+                                {
+                                    "tags",
+                                    new Regex("<[^>]+>",
+                                    RegexOptions.Compiled | RegexOptions.Singleline)
+                                },
+                                {
+                                    "lineBreaks",
+                                    new Regex(@"[\u000A\u000B\u000C\u000D\u2028\u2029\u0085]+",
+                                    RegexOptions.Compiled)
+                                },
+                                {
+                                    "multiSpaces",
+                                    new Regex("[ ]{2,}",
+                                    RegexOptions.Compiled)
+                                },
+                                {
+                                    "nonLetters",
+                                    new Regex("\\P{L}",
+                                    RegexOptions.Compiled)
+                                }
+                            };
+
             //Crawl();
             HTMLExterminator();
         }
@@ -133,8 +179,6 @@ namespace MyCrawler
             string noHTMLPath = @"..\..\Resources\NoHTML";
             //string noHTMLPath = @"C:\Users\Ejer\Documents\GitHub\P7_WI\MyCrawler\MyCrawler\Resources\NoHTML";
             
-            Regex regexRipHTML = new Regex(" < body(?:[ ][^>]*)?>(?<contents>.*?)</body>",
-                                 RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Singleline);
 
             string currentDoc;
             int i = 0;
@@ -144,16 +188,24 @@ namespace MyCrawler
                 using (StreamReader sr = new StreamReader(x))
                 {
                     currentDoc = sr.ReadToEnd();
-                    currentDoc = regexRipHTML.Replace(currentDoc, " ");
 
-                    //if (!File.Exists(noHTMLPath + @"\" + Path.GetFileName(x)))
-                    //{
-                        Console.WriteLine("entered");
-                        Console.WriteLine(noHTMLPath + @"\" + Path.GetFileName(x));
-                        //File.WriteAllText(@"C:\Users\Ejer\Documents\GitHub\P7_WI\MyCrawler\MyCrawler\Resources\NoHTML\text" + i, currentDoc);
-                        File.WriteAllText(noHTMLPath + @"\" + i +Path.GetFileName(x), currentDoc);
-                        System.Threading.Thread.Sleep(599);
-                    //}
+                    // Regexes by Tristan aka AngelOD
+                    currentDoc = this.Regexes["scripts"].Replace(currentDoc, " ");
+
+                    var bodyMatch = this.Regexes["body"].Match(currentDoc);
+                    if (bodyMatch.Groups.Count == 0)
+                    {
+                        continue;
+                    }
+                    var bodyText = bodyMatch.Groups["contents"].Value;
+                    bodyText = this.Regexes["styles"].Replace(bodyText, " ");
+                    bodyText = this.Regexes["tags"].Replace(bodyText, " ");
+                    bodyText = WebUtility.HtmlDecode(bodyText);
+                    bodyText = this.Regexes["lineBreaks"].Replace(bodyText, " ");
+                    bodyText = this.Regexes["nonLetters"].Replace(bodyText.ToLower(), " ");
+                    bodyText = this.Regexes["multiSpaces"].Replace(bodyText, " ");
+
+                    File.WriteAllText(noHTMLPath + @"\" + i +Path.GetFileName(x), bodyText);
 
                     sr.Close();
                     sr.Dispose();
